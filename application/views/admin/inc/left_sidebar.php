782d030842b386b7d460a2b9a84107ef41f1857f
etuna chichua <etunachichua@yahoo.com>
2016-07-21 18:32:15 +0400

    etuna

---
+++ application/views/admin/inc/left_sidebar.php
@@ -0,0 +1,93 @@
+<div id="sidebarbg" class="hidden-lg hidden-md hidden-sm hidden-xs"></div>
+            <!--Sidebar content-->
+            <div id="sidebar" class="page-sidebar hidden-lg hidden-md hidden-sm hidden-xs">
+
+                <!-- End search -->
+                <!-- Start .sidebar-inner -->
+                <div class="sidebar-inner">
+                    <!-- Start .sidebar-scrollarea -->
+                    <div class="sidebar-scrollarea">
+                        <div class="sidenav">
+                            <div class="sidebar-widget mb0">
+                                <h6 class="title mb0">ნავიგაცია</h6>
+                            </div>
+                            <!-- End .sidenav-widget -->
+                            <div class="mainnav">
+                                <ul>
+                                    <li><a href="<?=base_url()?>"><i class="s16 icomoon-icon-screen-2"></i><span class="txt">მთავარი</span> </a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="s16 icomoon-icon-stats-up"></i><span class="txt">ინვესტორები</span> <span class="notification red">6</span></a>
+                                        <ul class="sub">
+                                            <li><a href="charts-flot.html"><i class="s16 icomoon-icon-arrow-right-3"></i><span class="txt">Flot charts</span></a>
+                                            </li>
+                                            <li><a href="charts-rickshaw.html"><i class="s16 icomoon-icon-arrow-right-3"></i><span class="txt">Rickshaw charts</span></a>
+                                            </li>
+                                            <li><a href="charts-morris.html"><i class="s16 icomoon-icon-arrow-right-3"></i><span class="txt">Morris charts</span></a>
+                                            </li>
+                                            <li><a href="charts-chartjs.html"><i class="s16 icomoon-icon-arrow-right-3"></i><span class="txt">Chartjs</span></a>
+                                            </li>
+                                            <li><a href="charts-other.html"><i class="s16 icomoon-icon-arrow-right-3"></i><span class="txt">Other charts</span></a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="s16 icomoon-icon-list"></i><span class="txt">კომპანიები</span><span class="notification red">6</span></a>
+                                        <ul class="sub">
+                                            <li><a href="<?=base_url()?>admin/corps"><i class="s16 icomoon-icon-file"></i><span class="txt">შემოსული განაცხადები</span></a>
+                                            </li>
+                                            <li><a href="<?=base_url()?>admin/corps/regs"><i class="s16 icomoon-icon-file"></i><span class="txt">რეგისტრირებული</span></a>
+                                            </li>
+                                            <li><a href="forms-layouts.html"><i class="s16 icomoon-icon-file"></i><span class="txt">განაცხადები</span></a>
+                                                <ul class="sub">
+                                                    <li><a href="forms-wizard.html"><i class="s16 fa fa-magic"></i><span class="txt">აქტიური</span></a></li>
+                                                    <li><a href="forms-wizard.html"><i class="s16 fa fa-magic"></i><span class="txt">დასრულებული</span></a></li>
+                                                </ul>
+                                            </li>
+                                            
+                                            
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="s16 icomoon-icon-table-2"></i><span class="txt">ფუნქციები</span></a>
+                                        <ul class="sub">
+                                            <li><a href="<?=admin_url()?>pages"><i class="s16 icomoon-icon-arrow-right-3"></i><span class="txt">გვერდები</span></a>
+                                            </li>
+                                            <li><a href="<?=admin_url()?>admin"><i class="s16 icomoon-icon-arrow-right-3"></i><span class="txt">ადმინები</span></a>
+                                            </li>
+                                     <!--        <li><a href="<?=admin_url()?>pages"><i class="s16 icomoon-icon-arrow-right-3"></i><span class="txt">სოციალური</span></a>
+                                            </li> -->
+
+                                        </ul>
+                                    </li>
+
+                                    <li>
+                                        <a href="#"><i class="s16 icomoon-icon-table-2"></i><span class="txt">ფილმები</span></a>
+                                        <ul class="sub">
+                                            <li><a href="<?=admin_url()?>actors"><i class="s16 icomoon-icon-arrow-right-3"></i><span class="txt">მსახიობები</span></a>
+                                            </li>
+                                           
+                                    
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- End sidenav -->
+                        <div class="sidebar-widget">
+                            <h6 class="title">სტატისტიკა</h6>
+                            <div class="content">
+                                <div class="rightnow">
+                                    <ul class="list-unstyled">
+                                        <li><span class="number">34</span>ინვესტორი</li>
+                                        <li><span class="number">7</span>კომპანია</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End .sidenav-widget -->
+                    </div>
+                    <!-- End .sidebar-scrollarea -->
+                </div>
+                <!-- End .sidebar-inner -->
+            </div>
